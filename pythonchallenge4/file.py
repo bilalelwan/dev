590c4fc20d178ce478d3b3bf0d62aca5e77dea7c
Bilal Elwan <elwanbilalpro@gmail.com>
2024-10-24 13:45:53 +0300

    Delete pythonchallenge4 directory

--- pythonchallenge4/file.py
+++
@@ -1,6 +0,0 @@
-print("List generator")
-start = int(input("start at: "))
-end = int(input("end before: "))
-increment = int(input("increment between values: "))
-for value in range(start, end, increment):
-    print(value)
